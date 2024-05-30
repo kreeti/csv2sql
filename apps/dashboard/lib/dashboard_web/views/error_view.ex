e5688591c19d3c48250032f8e75bb4f702e3ffe9
bhargavsb <51400277+sbbhargav@users.noreply.github.com>
2024-05-30 18:49:17 +0530

    Unpack umbrella and remove dashboard application (#10)

--- apps/dashboard/lib/dashboard_web/views/error_view.ex
+++
@@ -1,7 +0,0 @@
-defmodule DashboardWeb.ErrorView do
-  use DashboardWeb, :view
-
-  def template_not_found(template, _assigns) do
-    Phoenix.Controller.status_message_from_template(template)
-  end
-end
--- config/config.exs
+++
@@ -1,2 +0,0 @@
-import Config
-import_config "../apps/dashboard/config/config.exs"
