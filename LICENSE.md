de8d772d8c68223d0f2d3b76ad0b1ec4fde4d8e0
bhargavsb <51400277+sbbhargav@users.noreply.github.com>
2024-05-30 12:34:24 +0530

    Update mix file to publish a package (#9)

--- LICENSE.md
+++ LICENSE.md
@@ -1,6 +1,6 @@
 MIT License
 
-Copyright (c) [2022] [Arpan Ghoshal]
+Copyright (c) [2024] [Arpan Ghoshal] [Kreeti Technologies]
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Csv2sql.Umbrella.MixProject do
   def project do
     [
       apps_path: "apps",
-      version: "3.0.0",
+      version: "4.0.0",
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
       deps: deps(),
@@ -18,7 +18,21 @@ defmodule Csv2sql.Umbrella.MixProject do
         csv2sql_only: [
           applications: [csv2sql: :permanent]
         ]
-      ]
+      ],
+      name: "Csv2sql",
+      description: "Csv2Sql is a blazing fast fully automated tool to load huge CSV files into a RDBMS.",
+      package: package(),
+      source_url: "https://github.com/kreeti/csv2sql"
+    ]
+  end
+
+  defp package() do
+    [
+      name: "csv2sql",
+      licenses: ["MIT"],
+      links: %{
+        "GitHub" => "https://github.com/kreeti/csv2sql"
+      }
     ]
   end
 
@@ -28,7 +42,8 @@ defmodule Csv2sql.Umbrella.MixProject do
 
   defp deps do
     [
-      {:credo, "~> 1.7"}
+      {:credo, "~> 1.7"},
+      {:ex_doc, "~> 0.33.0"}
     ]
   end
 end
